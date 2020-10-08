56a7035cfe6615305dd84881ffa1ecc5ce4e7d2b
Dennis Franke <franke@alchemisten.de>
2020-10-08 15:09:09 +0200

    small changes

--- generators/index.js
+++ generators/index.js
@@ -1,5 +1,7 @@
 const Generator = require("yeoman-generator");
 const latestVersion = require('latest-version');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class extends Generator {
   constructor(args, opts) {
@@ -12,7 +14,13 @@ module.exports = class extends Generator {
         type: "input",
         name: "projectName",
         message: "Project name",
-        default: this.appname.replace(/\s+/g, '-') // Default to current folder name
+        validate: function(projectName){
+          const isPascalCase = /^[A-Z][a-z]+(?:[A-Z][a-z]+)*$/.test(projectName);
+          if(!isPascalCase) {
+            return "Your project name must be specified in PascalCase!";
+          }
+          return true;
+        }
       }
     ]);
   }
@@ -24,13 +32,29 @@ module.exports = class extends Generator {
     console.log(await latestVersion('@aws-cdk/core'));
   }
 
-  wirting() {
+  writing() {
     const { projectName } = this.answers;
+    const projectNameLispCase = projectName
+        .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+        .replace(/([A-Z])([A-Z])(?=[a-z])/g, '$1-$2')
+        .toLowerCase();
 
     this.fs.copyTpl(
       this.templatePath('package.json.ejs'),
       this.destinationPath('package.json'),
       { projectName }
+    );
+
+    this.fs.copyTpl(
+        this.templatePath('cdk-stack/bin/cdk.ts.ejs'),
+        this.destinationPath(`cdk-stack/bin/${projectNameLispCase}.ts`),
+        { projectName, projectNameLispCase }
+    );
+
+    this.fs.copyTpl(
+        this.templatePath('cdk-stack/lib/stack.ts.ejs'),
+        this.destinationPath(`cdk-stack/lib/${projectNameLispCase}-stack.ts`),
+        { projectName }
     );
   }
 };
